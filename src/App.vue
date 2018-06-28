c731ae66d1b5d1b476e87739ed952f600da9e934
skokova <skokova@reksoft.ru>
2018-06-28 15:15:42 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:create-todo="addTodo"></create-todo>
+  </div>
+</template>
+
+<script>
+import CreateTodo from './components/CreateTodo';
+import TodoList from './components/TodoList';
+
+export default {
+  name: 'app',
+  components: {
+    CreateTodo,
+    TodoList,
+  },
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: true,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: false,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }],
+    }
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.push(todo);
+    },
+  },
+}
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="ui basic content center aligned segment">
+        <p>Comleted Tasks: {{ todos.filter(todo => {return todo.done === true}).length }}</p>
+        <p>Pending Tasks: {{ todos.filter(todo => {return todo.done === false}).length }}</p>
+        <todo v-for="todo in todos"
+              v-bind:todo="todo"
+              v-on:delete-todo="deleteTodo"
+              v-on:complete-todo="completeTodo"
+        ></todo>
+    </div>
+</template>
+
+<script>
+import Todo from './Todo.vue';
+
+export default {
+    props: ['todos'],
+    components: {
+        Todo,
+    },
+    methods: {
+        deleteTodo(todo) {
+          const todoIndex = this.todos.indexOf(todo);
+          this.todos.splice(todoIndex, 1);
+        },
+        completeTodo(todo) {
+          const todoIndex = this.todos.indexOf(todo);
+          this.todos[todoIndex].done = true;
+        },
+    },
+};
+</script>
